142e6ad5329be05db0ec971727c45f2e88510087
Anand Raj <anandrajj10@gmail.com>
2022-01-27 08:12:27 +0530

    ++

---
+++ callback.js
@@ -0,0 +1,30 @@
+/* A callback function is a function that is passed to another function as 
+an argument and is executed after some operation has been completed.*/
+
+function modifyArray(arr, callback) {
+  // do something to arr here
+  arr.push(100);
+  // then execute the callback function that was passed
+  callback();
+}
+
+var arr = [1, 2, 3, 4, 5];
+
+modifyArray(arr, function () {
+  console.log("array has been modified", arr);
+});
+
+
+/* --------------Given a string, reverse each word in the sentence --------------- */
+
+var string = "Welcome to this Javascript Guide!";
+
+// Output becomes !ediuG tpircsavaJ siht ot emocleW
+var reverseEntireSentence = reverseBySeparator(string, "");
+
+// Output becomes emocleW ot siht tpircsavaJ !ediuG
+var reverseEachWord = reverseBySeparator(reverseEntireSentence, " ");
+
+function reverseBySeparator(string, separator) {
+  return string.split(separator).reverse().join(separator);
+}
