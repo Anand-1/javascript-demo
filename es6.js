5f059be422785c1b971130eaac86722f900455d2
Anand Raj <anandrajj10@gmail.com>
2022-01-27 07:59:57 +0530

    es6

---
+++ es6.js
@@ -0,0 +1,18 @@
+/* Arrow function provides a shorter syntax to write more concise function expression
+  - if block syntax is used return keyword is needed else not
+  - No function keyword is used 
+*/
+const lunchMenu = pizza=> `I'm going to eat a ${pizza} for lunch`;
+console.log( lunchMenu("onion") );
+
+/*--------------------------------------------------------------------------------------*/
+/* Template Literals : String literals with embedded expressions are called template literals . */
+
+let num1 = 2;
+let num2 = 3;
+const fullName = `${num1 + num2}`;
+console.log(fullName);
+
+/*--------------------------------------------------------------------------------------*/
+/*  */
+
